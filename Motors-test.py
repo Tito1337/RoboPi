ed310bffd7a8ea93349e9e5d25aeb2f89e6d1616
Tito1337 <tito@webtito.be>
2015-09-23 15:35:10 +0200

    Of course it didn't work at first try

--- Motors-test.py
+++ Motors-test.py
@@ -34,7 +34,10 @@ GPIO.output(Motor2B,GPIO.HIGH)
 sleep(2)
  
 print "Now stop"
-GPIO.output(Motor1E,GPIO.LOW)
-GPIO.output(Motor2E,GPIO.LOW)
+GPIO.output(Motor1A,GPIO.LOW)
+GPIO.output(Motor1B,GPIO.LOW)
+ 
+GPIO.output(Motor2A,GPIO.LOW)
+GPIO.output(Motor2B,GPIO.LOW)
  
 GPIO.cleanup()
