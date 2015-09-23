43597e0b76cdc7dfbb7f86b6d4c9f4acc7d2b0c5
Tito1337 <tito@webtito.be>
2015-09-23 16:00:12 +0200

    Script en fonctions

--- Motors-test.py
+++ Motors-test.py
@@ -1,43 +1,59 @@
 import RPi.GPIO as GPIO
 from time import sleep
+
+# Configuration
+Motor1A = 23
+Motor1B = 21
  
-GPIO.setmode(GPIO.BOARD)
+Motor2A = 16
+Motor2B = 18
  
-Motor1A = 16
-Motor1B = 18
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+
+	GPIO.setup(Motor1A,GPIO.OUT)
+	GPIO.setup(Motor1B,GPIO.OUT)
  
-Motor2A = 23
-Motor2B = 21
+	GPIO.setup(Motor2A,GPIO.OUT)
+	GPIO.setup(Motor2B,GPIO.OUT)
+
+def cleanup():
+	GPIO.cleanup()
  
-GPIO.setup(Motor1A,GPIO.OUT)
-GPIO.setup(Motor1B,GPIO.OUT)
+def M1forward():
+	GPIO.output(Motor1A,GPIO.HIGH)
+	GPIO.output(Motor1B,GPIO.LOW)
+
+def M2forward():
+	GPIO.output(Motor2A,GPIO.HIGH)
+	GPIO.output(Motor2B,GPIO.LOW)
+
+def M1backwards():
+	GPIO.output(Motor1A,GPIO.LOW)
+	GPIO.output(Motor1B,GPIO.HIGH)
+
+def M2backwards():
+	GPIO.output(Motor2A,GPIO.LOW)
+	GPIO.output(Motor2B,GPIO.HIGH)
  
-GPIO.setup(Motor2A,GPIO.OUT)
-GPIO.setup(Motor2B,GPIO.OUT)
+def M1stop():
+	GPIO.output(Motor1A,GPIO.LOW)
+	GPIO.output(Motor1B,GPIO.LOW)
  
-print "Going forwards"
-GPIO.output(Motor1A,GPIO.HIGH)
-GPIO.output(Motor1B,GPIO.LOW)
+def M2stop():
+	GPIO.output(Motor2A,GPIO.LOW)
+	GPIO.output(Motor2B,GPIO.LOW)
  
-GPIO.output(Motor2A,GPIO.HIGH)
-GPIO.output(Motor2B,GPIO.LOW)
- 
-sleep(2)
- 
-print "Going backwards"
-GPIO.output(Motor1A,GPIO.LOW)
-GPIO.output(Motor1B,GPIO.HIGH)
- 
-GPIO.output(Motor2A,GPIO.LOW)
-GPIO.output(Motor2B,GPIO.HIGH)
- 
-sleep(8)
- 
-print "Now stop"
-GPIO.output(Motor1A,GPIO.LOW)
-GPIO.output(Motor1B,GPIO.LOW)
- 
-GPIO.output(Motor2A,GPIO.LOW)
-GPIO.output(Motor2B,GPIO.LOW)
- 
-GPIO.cleanup()
+if __name__ == '__main__':
+	setup()
+
+	M1forward()
+	M2forward()
+	sleep(2)
+	M1backwards()
+	M2backwards()
+	sleep(2)
+	M1stop()
+	M2stop()
+
+	cleanup()
