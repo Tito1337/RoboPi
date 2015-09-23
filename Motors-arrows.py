2dd1c4dc977abfe0a2ee9cad02ce9654be5eebf7
Tito1337 <tito@webtito.be>
2015-09-23 16:44:00 +0200

    Indentation in spaces

--- Motors-arrows.py
+++ Motors-arrows.py
@@ -12,75 +12,75 @@ Motor2A = 16
 Motor2B = 18
  
 def setup():
-	GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)
 
-	GPIO.setup(Motor1A,GPIO.OUT)
-	GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
  
-	GPIO.setup(Motor2A,GPIO.OUT)
-	GPIO.setup(Motor2B,GPIO.OUT)
+    GPIO.setup(Motor2A,GPIO.OUT)
+    GPIO.setup(Motor2B,GPIO.OUT)
 
 def cleanup():
-	GPIO.cleanup()
+    GPIO.cleanup()
  
 def M1forward():
-	GPIO.output(Motor1A,GPIO.HIGH)
-	GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
 
 def M2forward():
-	GPIO.output(Motor2A,GPIO.HIGH)
-	GPIO.output(Motor2B,GPIO.LOW)
+    GPIO.output(Motor2A,GPIO.HIGH)
+    GPIO.output(Motor2B,GPIO.LOW)
 
 def M1backwards():
-	GPIO.output(Motor1A,GPIO.LOW)
-	GPIO.output(Motor1B,GPIO.HIGH)
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.HIGH)
 
 def M2backwards():
-	GPIO.output(Motor2A,GPIO.LOW)
-	GPIO.output(Motor2B,GPIO.HIGH)
+    GPIO.output(Motor2A,GPIO.LOW)
+    GPIO.output(Motor2B,GPIO.HIGH)
  
 def M1stop():
-	GPIO.output(Motor1A,GPIO.LOW)
-	GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.LOW)
  
 def M2stop():
-	GPIO.output(Motor2A,GPIO.LOW)
-	GPIO.output(Motor2B,GPIO.LOW)
+    GPIO.output(Motor2A,GPIO.LOW)
+    GPIO.output(Motor2B,GPIO.LOW)
 
 def getKey():
-	screen = curses.initscr()
-	try:
-	    curses.noecho()
-	    curses.curs_set(0)
-	    screen.keypad(1)
-	    event = screen.getch()
-	finally:
-	    curses.endwin()
-	return event
+    screen = curses.initscr()
+    try:
+        curses.noecho()
+        curses.curs_set(0)
+        screen.keypad(1)
+        event = screen.getch()
+    finally:
+        curses.endwin()
+    return event
 
 if __name__ == '__main__':
-	setup()
+    setup()
 
-	while True:
-		M1stop()
-		M2stop()
-		event = getKey()
-		if event == curses.KEY_LEFT:
-			print("Left Arrow Key pressed")
-			M1backwards()
-			M2forward()
-		elif event == curses.KEY_RIGHT:
-			print("Right Arrow Key pressed")
-			M1forward()
-			M2backwards()
-		elif event == curses.KEY_UP:
-			M1forward()
-			M2forward()
-		elif event == curses.KEY_DOWN:
-			M1backwards()
-			M2backwards()
-		else:
-			print(event)
-		sleep(0.5)
+    while True:
+        M1stop()
+        M2stop()
+        event = getKey()
+        if event == curses.KEY_LEFT:
+            print("Left Arrow Key pressed")
+            M1backwards()
+            M2forward()
+        elif event == curses.KEY_RIGHT:
+            print("Right Arrow Key pressed")
+            M1forward()
+            M2backwards()
+        elif event == curses.KEY_UP:
+            M1forward()
+            M2forward()
+        elif event == curses.KEY_DOWN:
+            M1backwards()
+            M2backwards()
+        else:
+            print(event)
+        sleep(0.5)
 
-	cleanup()
+    cleanup()
--- Proximity-sensors.py
+++ Proximity-sensors.py
@@ -14,50 +14,50 @@ Motor2B = 18
 Sensor2 = 20
  
 def setup():
-	GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)
 
-	GPIO.setup(Motor1A,GPIO.OUT)
-	GPIO.setup(Motor1B,GPIO.OUT)
-	GPIO.setup(Sensor1,GPIO.IN)
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Sensor1,GPIO.IN)
  
-	GPIO.setup(Motor2A,GPIO.OUT)
-	GPIO.setup(Motor2B,GPIO.OUT)
-	GPIO.setup(Sensor2,GPIO.IN)
+    GPIO.setup(Motor2A,GPIO.OUT)
+    GPIO.setup(Motor2B,GPIO.OUT)
+    GPIO.setup(Sensor2,GPIO.IN)
 
 def cleanup():
-	GPIO.cleanup()
+    GPIO.cleanup()
  
 def M1forward():
-	GPIO.output(Motor1A,GPIO.HIGH)
-	GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
 
 def M2forward():
-	GPIO.output(Motor2A,GPIO.HIGH)
-	GPIO.output(Motor2B,GPIO.LOW)
+    GPIO.output(Motor2A,GPIO.HIGH)
+    GPIO.output(Motor2B,GPIO.LOW)
 
 def M1backwards():
-	GPIO.output(Motor1A,GPIO.LOW)
-	GPIO.output(Motor1B,GPIO.HIGH)
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.HIGH)
 
 def M2backwards():
-	GPIO.output(Motor2A,GPIO.LOW)
-	GPIO.output(Motor2B,GPIO.HIGH)
+    GPIO.output(Motor2A,GPIO.LOW)
+    GPIO.output(Motor2B,GPIO.HIGH)
  
 def M1stop():
-	GPIO.output(Motor1A,GPIO.LOW)
-	GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.LOW)
  
 def M2stop():
-	GPIO.output(Motor2A,GPIO.LOW)
-	GPIO.output(Motor2B,GPIO.LOW)
+    GPIO.output(Motor2A,GPIO.LOW)
+    GPIO.output(Motor2B,GPIO.LOW)
 
 if __name__ == '__main__':
-	setup()
+    setup()
 
-	try:
-		while True:
-        	time.sleep(10)
-	except KeyboardInterrupt:
-    	print 'Exiting...'
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print 'Exiting...'
 
-	cleanup()
+    cleanup()
